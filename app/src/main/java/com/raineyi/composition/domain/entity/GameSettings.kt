a269eb4dcfa13b12af50bf93c4e9641cfe843b8c
raine <rainey888@mail.ru>
2024-01-08 23:08:54 +0700

    Created domain-layer - Added 4 entities(Level, GameSettings, Question and GameResult) - Added 2 use cases (GetGameSettingsUseCase and GenerateQuestionUseCase) - Added repository

---
+++ app/src/main/java/com/raineyi/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,8 @@
+package com.raineyi.composition.domain.entity
+
+class GameSettings (
+    val maxSumValue: Int,
+    val minCountOfRightAnswers: Int,
+    val minPercentOfRightAnswers: Int,
+    val gameTimeInSeconds: Int
+)
