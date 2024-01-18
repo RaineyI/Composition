64f1671bd11af1c33f35995e56115209b8bb24a1
raine <rainey888@mail.ru>
2024-01-18 19:59:02 +0700

    Added navigation between fragments

--- app/src/main/java/com/raineyi/composition/domain/entity/Level.kt
+++ app/src/main/java/com/raineyi/composition/domain/entity/Level.kt
@@ -1,5 +1,5 @@
 package com.raineyi.composition.domain.entity
 
-enum class Level {
+enum class Level  {
     TEST, EASY, NORMAL, HARD
 }
--- app/src/main/java/com/raineyi/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/raineyi/composition/presentation/GameFragment.kt
@@ -7,11 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import com.raineyi.composition.R
 import com.raineyi.composition.databinding.FragmentGameBinding
+import com.raineyi.composition.domain.entity.GameResult
+import com.raineyi.composition.domain.entity.GameSettings
+import com.raineyi.composition.domain.entity.Level
+
 
 class GameFragment : Fragment() {
+    private lateinit var level: Level
+
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,8 +32,39 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvQuestion.setOnClickListener {
+            launchGameFinishedFragment(GameResult(true, 10, 10, GameSettings(10, 10, 50, 50)))
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.getInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+        private const val KEY_LEVEL = "level"
+        const val NAME = "GameFragment"
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
